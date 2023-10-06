fdc84c44e3c36c95130278eb33744d7d69342bee
fire51 <firehiwotbelay01@gmail.com>
2023-10-05 18:01:42 -0700

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+if__name__==__"main__":
+    """Print the hidden alphabet order"""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2]!="__"
+            print(name)
