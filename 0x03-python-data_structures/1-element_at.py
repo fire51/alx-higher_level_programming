d6ee1cd3abb40ddaa7f9c997d34ec5c66dc9f57b
fire51 <firehiwotbelay01@gmail.com>
2023-10-10 15:16:21 -0700

    0x03-python-data_struchure

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#1-element_at.py
+
+def element_at(my_list, idx):
+    """retrive an element frome alist"""
+    if idx<0oridx>len((my_list)-1):
+        return None
+    return (my_list[idx])
